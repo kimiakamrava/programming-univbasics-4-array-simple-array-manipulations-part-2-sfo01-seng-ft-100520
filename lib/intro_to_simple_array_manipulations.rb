28d5452e609e79f7b9c15fb82ce52d178e0d3a18
kimia kamrava <kimiakamrava93@gmail.com>
2020-09-17 21:04:46 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,13 @@
+
+def using_concat (my_favorite_things,more_favs)
+   my_favorite_things.concat(more_favs)
+ end
+  
+  
+  def using_insert (array , element)
+    array[4].insert(element)
+end
+
+  
+
+  
